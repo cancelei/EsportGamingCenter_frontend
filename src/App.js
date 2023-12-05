8c6d59d34227374e706c24e13e116b5d11378fba
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-12-05 16:16:57 -0500

    Initiate code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './Components/Login'; // Asegúrate de que este path sea correcto
+import Register from './Components/Register'; // Asegúrate de que este path sea correcto
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            {/* Redirige la ruta raíz a /login */}
+            <Route path="/" element={<Navigate to="/login" replace />} />
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
