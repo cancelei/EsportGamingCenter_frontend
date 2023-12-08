eb039c4482b3cf9bcf78a54d8776855910f0ccfe
Luis Emilio Rojas <luisemiliorojas@hotmail.com>
2023-12-08 16:01:00 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux'; // Importar Provider de react-redux
+import store from './redux/store'; // Importar tu store desde la ubicación correcta
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -7,11 +9,11 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Envolver App con Provider y pasar el store */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
