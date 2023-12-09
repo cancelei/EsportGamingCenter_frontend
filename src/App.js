aa14f23f9fbcf6a2abb47fe4654946cb09b3454e
cancelei <glauber.bannwart@gmail.com>
2023-12-09 12:45:17 -0300

    Add GamesListComponent and background image

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import DeleteGame from './Components/games/DeleteGame';
 import Reservations from './Components/reservations/Reservations';
 import GameDetails from './Components/details/GameDetails';
 import './App.css';
+import GamesListComponent from './Components/games/GamesListComponent';
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
         {/* <Route element={<RequireAuth />}> */}
         <Route exact path="/addGame" element={<AddGamesForm />} />
         <Route path="/games" element={<MainPage />} />
+        <Route path="/gamelist" element={<GamesListComponent />} />
         <Route path="/details/:gameId" element={<GameDetails />} />
         <Route path="/games/delete" element={<DeleteGame />} />
         <Route path="/reservations" element={<Reservations />} />
