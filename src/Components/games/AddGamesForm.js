b39134f93150b5b8d2a5c7f8b5b1dd1a9e11654f
Gardimy Charles <gardimycharles889@gmail.com>
2023-12-12 07:50:50 -0500

    fix the background image

--- src/Components/games/AddGamesForm.js
+++ src/Components/games/AddGamesForm.js
@@ -36,37 +36,39 @@ function AddGameForm() {
   };
 
   return (
-    <div className="formcontainer">
-      <h2>Add New Game</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="title">Title:</label>
-          <input
-            type="text"
-            id="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="description">Description:</label>
-          <textarea
-            id="description"
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor="imageUrl">Image URL:</label>
-          <input
-            type="text"
-            id="imageUrl"
-            value={imageUrl}
-            onChange={(e) => setImageUrl(e.target.value)}
-          />
-        </div>
-        <button type="submit">Add Game</button>
-      </form>
+    <div className="body-background">
+      <div className="formcontainer">
+        <h2>Add New Game</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="title">Title:</label>
+            <input
+              type="text"
+              id="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="description">Description:</label>
+            <textarea
+              id="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+          <div>
+            <label htmlFor="imageUrl">Image URL:</label>
+            <input
+              type="text"
+              id="imageUrl"
+              value={imageUrl}
+              onChange={(e) => setImageUrl(e.target.value)}
+            />
+          </div>
+          <button type="submit">Add Game</button>
+        </form>
+      </div>
     </div>
   );
 }
