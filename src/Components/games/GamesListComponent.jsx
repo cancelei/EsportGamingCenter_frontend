17fcfa841d7db108b7910d44b4413b6755fef547
cancelei <glauber.bannwart@gmail.com>
2023-12-13 14:58:25 -0300

    Refactor components and add Navbar

--- src/Components/games/GamesListComponent.jsx
+++ src/Components/games/GamesListComponent.jsx
@@ -3,53 +3,56 @@ import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import './GamesListComponent.css'; // Importar el archivo CSS
 import background from '../../assets/background.webp';
+import Navbar from '../Navbar';
 
 function GamesListComponent() {
-    const [games, setGames] = useState([]);
-    const navigate = useNavigate();
+  const [games, setGames] = useState([]);
+  const navigate = useNavigate();
 
-    const fetchGames = async () => {
-        try {
-            const response = await axios.get('http://localhost:3000/api/games');
-            setGames(response.data);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const fetchGames = async () => {
+    try {
+      const response = await axios.get('http://localhost:3000/api/games');
+      setGames(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    useEffect(() => {
-        fetchGames();
-    }, []);
+  useEffect(() => {
+    fetchGames();
+  }, []);
 
-    const deleteGame = async (gameId, event) => {
-        event.stopPropagation(); // Previene la propagación del evento al elemento padre
-        try {
-            await axios.delete(`http://localhost:3000/api/games/${gameId}`);
-            fetchGames();
-        } catch (error) {
-            console.error(error);
-        }
-    };
+  const deleteGame = async (gameId, event) => {
+    event.stopPropagation(); // Previene la propagación del evento al elemento padre
+    try {
+      await axios.delete(`http://localhost:3000/api/games/${gameId}`);
+      fetchGames();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
-    const handleGameClick = (gameId) => {
-        navigate(`/details/${gameId}`);
-    };
+  const handleGameClick = (gameId) => {
+    navigate(`/details/${gameId}`);
+  };
 
-
-    return (
+  return (
+    <>
+      <Navbar />
       <div className="games-list-container" style={{ backgroundImage: `url(${background})` }}>
-          <button className="new-game-button" onClick={() => navigate('/addgame')}>New Game</button>
-          <ul className="games-list">
-              {games.map((game) => (
-                  <li key={game.id} className="game-list-item" onClick={() => handleGameClick(game.id)}>
-                      <h3 className="game-header">{game.title}</h3>
-                      <p className="game-description">{game.description}</p>
-                      <img className="game-image" src={game.image_url} alt={game.title} />
-                      <button className="delete-button" onClick={(e) => deleteGame(game.id, e)}>Delete</button>
-                  </li>
-              ))}
-          </ul>
+        <button type="button" className="new-game-button" onClick={() => navigate('/addgame')}>New Game</button>
+        <ul className="games-list">
+          {games.map((game) => (
+            <li key={game.id} className="game-list-item" onClick={() => handleGameClick(game.id)}>
+              <h3 className="game-header">{game.title}</h3>
+              <p className="game-description">{game.description}</p>
+              <img className="game-image" src={game.image_url} alt={game.title} />
+              <button className="delete-button" onClick={(e) => deleteGame(game.id, e)}>Delete</button>
+            </li>
+          ))}
+        </ul>
       </div>
+    </>
   );
 }
 
--- src/Components/reservations/ReservationForm.jsx
+++ src/Components/reservations/ReservationForm.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
+import Navbar from '../Navbar';
 
 function ReservationForm() {
   const [reservationDate, setReservationDate] = useState('');
@@ -21,7 +22,7 @@ function ReservationForm() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-  
+
     const reservationData = {
       reservation: {
         game_id: gameId,
@@ -54,49 +55,52 @@ function ReservationForm() {
   };
 
   return (
-    <div className="body-background">
-      <div className="formcontainer">
-        <h2>Make New Reservation</h2>
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="date">
-              Reservation Date:
-              <input
-                type="date"
-                value={reservationDate}
-                onChange={(e) => setReservationDate(e.target.value)}
-              />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="setup_config">
-              PC Setup:
-              <input
-                type="text"
-                value={setupConfig}
-                onChange={(e) => setSetupConfig(e.target.value)}
-              />
-            </label>
-          </div>
-          <div>
-            <label htmlFor="platform">
-              Platform:
-              <select value={platform} onChange={(e) => setPlatform(e.target.value)}>
-                <option value="">Select Platform</option>
-                <option value="PC">PC</option>
-                <option value="Xbox Series S/X">Xbox Series S/X</option>
-                <option value="PS4">PS4</option>
-                <option value="PS5">PS5</option>
-                <option value="Nintendo Switch">Nintendo Switch</option>
-                <option value="iOS">iOS</option>
-                <option value="Android">Android</option>
-              </select>
-            </label>
-          </div>
-          <button type="submit">Reserve</button>
-        </form>
+    <>
+      <Navbar />
+      <div className="body-background">
+        <div className="formcontainer">
+          <h2>Make New Reservation</h2>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="date">
+                Reservation Date:
+                <input
+                  type="date"
+                  value={reservationDate}
+                  onChange={(e) => setReservationDate(e.target.value)}
+                />
+              </label>
+            </div>
+            <div>
+              <label htmlFor="setup_config">
+                PC Setup:
+                <input
+                  type="text"
+                  value={setupConfig}
+                  onChange={(e) => setSetupConfig(e.target.value)}
+                />
+              </label>
+            </div>
+            <div>
+              <label htmlFor="platform">
+                Platform:
+                <select value={platform} onChange={(e) => setPlatform(e.target.value)}>
+                  <option value="">Select Platform</option>
+                  <option value="PC">PC</option>
+                  <option value="Xbox Series S/X">Xbox Series S/X</option>
+                  <option value="PS4">PS4</option>
+                  <option value="PS5">PS5</option>
+                  <option value="Nintendo Switch">Nintendo Switch</option>
+                  <option value="iOS">iOS</option>
+                  <option value="Android">Android</option>
+                </select>
+              </label>
+            </div>
+            <button type="submit">Reserve</button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
